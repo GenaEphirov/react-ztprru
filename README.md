3c1b57ebad4c691f9c37b8a0466ae35082ccbbbe
GenaEphirov <self.education.1411@gmail.com>
2022-07-27 14:01:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ztprru
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ztprru)
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Header.css'
+
+const Header = () => {
+  return(
+    <header>
+      <div className={s.header}></div>
+    </header>
+  )
+}
+
+export default Header
