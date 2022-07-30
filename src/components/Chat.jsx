b35eb3711d738c7e617b27264709bd5b5b0ce5f3
GenaEphirov <self.education.1411@gmail.com>
2022-07-30 17:13:47 +0300

    2 files modified

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,10 +3,16 @@ import s from './Chat.module.css'
 import Post from './Post'
 
 const Chat = () => {
+  let chatData = [
+    {userID: 1, messageID: 1, name: 'Andrey', message: 'Hello, Bill!', date: '26.07.2022'},
+    {userID: 2, messageID: 2, name: 'Bill', message: 'How is it going?', date: '27.07.2022'},
+  ]
+  let Posts = chatData.map( (elem) => {
+    return <Post name={elem.name} message={elem.message} date={elem.date} />
+  })
   return (
     <div className={s.container}>
-      <Post name='Andrey' message='Hello, Bill!' date='26.07.2022'/>
-      <Post name='Bill' message='How is it going?' date='27.07.2022'/>
+      {Posts}
     </div>
   )
 }
